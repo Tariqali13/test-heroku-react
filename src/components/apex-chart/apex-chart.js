c8d98f5d9cb93d672bb93f56f67479073310707c
Tariqali13 <Tariqali99.ta@gmail.com>
2021-12-28 20:54:49 +0500

    fix

--- src/components/apex-chart/apex-chart.js
+++ src/components/apex-chart/apex-chart.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import Chart from 'react-apexcharts';
+import Chart from 'react-apexcharts'
 
 const ApexChart = (props) => {
     const {options, series, type, height} = props;
     return (
-        <React.Fragment>
-            <Chart
-                options={options}
-                series={series}
-                type={type}
-                width="100%"
-                height={height || 250}
-            />
-        </React.Fragment>
+        <Chart
+            options={options}
+            series={series}
+            type={type}
+            width="100%"
+            height={height || 250}
+        />
     );
 };
 

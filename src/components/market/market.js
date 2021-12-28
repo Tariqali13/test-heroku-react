3953bb383a527c3a366aef23cf88e5d2a7419c36
Tariqali13 <Tariqali99.ta@gmail.com>
2021-12-28 16:23:33 +0500

    test

--- src/components/market/market.js
+++ src/components/market/market.js
@@ -26,17 +26,17 @@ const Market = () => {
                                 <div className="tab-content tab-content-basic">
                                     <div className="tab-pane fade show active" id="overview" role="tabpanel"
                                          aria-labelledby="overview">
-                                        <Stats/>
-                                        <LineChart/>
-                                        <div className="row">
-                                            <TopSmallChart heading="Top Buyers"/>
-                                            <TopSmallChart heading="Top Sellers"/>
-                                            <TopSmallChart heading="Top Movers"/>
-                                        </div>
-                                        <div className="row mb-3">
-                                            <HundredTrades/>
-                                        </div>
-                                        <TopCharts />
+                                        {/*<Stats/>*/}
+                                        {/*<LineChart/>*/}
+                                        {/*<div className="row">*/}
+                                        {/*    <TopSmallChart heading="Top Buyers"/>*/}
+                                        {/*    <TopSmallChart heading="Top Sellers"/>*/}
+                                        {/*    <TopSmallChart heading="Top Movers"/>*/}
+                                        {/*</div>*/}
+                                        {/*<div className="row mb-3">*/}
+                                        {/*    <HundredTrades/>*/}
+                                        {/*</div>*/}
+                                        {/*<TopCharts />*/}
                                     </div>
                                 </div>
                             </div>

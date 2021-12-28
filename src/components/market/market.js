6a4735c66fa22d4af889c8f824aed2f6af250f18
Tariqali13 <Tariqali99.ta@gmail.com>
2021-12-28 16:42:18 +0500

    fix

--- src/components/market/market.js
+++ src/components/market/market.js
@@ -2,10 +2,10 @@ import React from 'react';
 import MainTemplate from '../layouts/main-template';
 import {
     Stats,
-    LineChart,
-    TopSmallChart,
-    HundredTrades,
-    TopCharts
+    // LineChart,
+    // TopSmallChart,
+    // HundredTrades,
+    // TopCharts
 } from './components';
 
 const Market = () => {
@@ -23,16 +23,16 @@ const Market = () => {
                                     <div className="tab-pane fade show active" id="overview" role="tabpanel"
                                          aria-labelledby="overview">
                                         <Stats/>
-                                        <LineChart/>
-                                        <div className="row">
-                                            <TopSmallChart heading="Top Buyers"/>
-                                            <TopSmallChart heading="Top Sellers"/>
-                                            <TopSmallChart heading="Top Movers"/>
-                                        </div>
-                                        <div className="row mb-3">
-                                            <HundredTrades/>
-                                        </div>
-                                        <TopCharts />
+                                        {/*<LineChart/>*/}
+                                        {/*<div className="row">*/}
+                                        {/*    <TopSmallChart heading="Top Buyers"/>*/}
+                                        {/*    <TopSmallChart heading="Top Sellers"/>*/}
+                                        {/*    <TopSmallChart heading="Top Movers"/>*/}
+                                        {/*</div>*/}
+                                        {/*<div className="row mb-3">*/}
+                                        {/*    <HundredTrades/>*/}
+                                        {/*</div>*/}
+                                        {/*<TopCharts />*/}
                                     </div>
                                 </div>
                             </div>

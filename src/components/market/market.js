e0616d966b3e9d2092106afcb6f74b428b856ac7
Tariqali13 <Tariqali99.ta@gmail.com>
2021-12-28 16:38:47 +0500

    test

--- src/components/market/market.js
+++ src/components/market/market.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import MainTemplate from '../layouts/main-template';
+import {
+    Stats,
+    LineChart,
+    TopSmallChart,
+    HundredTrades,
+    TopCharts
+} from './components';
 
 const Market = () => {
 
@@ -15,17 +22,17 @@ const Market = () => {
                                 <div className="tab-content tab-content-basic">
                                     <div className="tab-pane fade show active" id="overview" role="tabpanel"
                                          aria-labelledby="overview">
-                                        {/*<Stats/>*/}
-                                        {/*<LineChart/>*/}
-                                        {/*<div className="row">*/}
-                                        {/*    <TopSmallChart heading="Top Buyers"/>*/}
-                                        {/*    <TopSmallChart heading="Top Sellers"/>*/}
-                                        {/*    <TopSmallChart heading="Top Movers"/>*/}
-                                        {/*</div>*/}
-                                        {/*<div className="row mb-3">*/}
-                                        {/*    <HundredTrades/>*/}
-                                        {/*</div>*/}
-                                        {/*<TopCharts />*/}
+                                        <Stats/>
+                                        <LineChart/>
+                                        <div className="row">
+                                            <TopSmallChart heading="Top Buyers"/>
+                                            <TopSmallChart heading="Top Sellers"/>
+                                            <TopSmallChart heading="Top Movers"/>
+                                        </div>
+                                        <div className="row mb-3">
+                                            <HundredTrades/>
+                                        </div>
+                                        <TopCharts />
                                     </div>
                                 </div>
                             </div>

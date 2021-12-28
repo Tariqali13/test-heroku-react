96ba9d10c5bbccb83c51aa5126aa2687025c11bc
Tariqali13 <Tariqali99.ta@gmail.com>
2021-12-28 16:50:06 +0500

    test

--- src/components/market/market.js
+++ src/components/market/market.js
@@ -12,35 +12,27 @@ const Market = () => {
 
     return (
         <MainTemplate>
-
-
             <div className="content-wrapper">
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-12">
                             <div className="home-tab">
-                                <div className="tab-content tab-content-basic">
-                                    <div className="tab-pane fade show active" id="overview" role="tabpanel"
-                                         aria-labelledby="overview">
-                                        {/*<Stats/>*/}
-                                        <LineChart/>
-                                        {/*<div className="row">*/}
-                                        {/*    <TopSmallChart heading="Top Buyers"/>*/}
-                                        {/*    <TopSmallChart heading="Top Sellers"/>*/}
-                                        {/*    <TopSmallChart heading="Top Movers"/>*/}
-                                        {/*</div>*/}
-                                        {/*<div className="row mb-3">*/}
-                                        {/*    <HundredTrades/>*/}
-                                        {/*</div>*/}
-                                        {/*<TopCharts />*/}
-                                    </div>
-                                </div>
+                                {/*<Stats/>*/}
+                                {/*<LineChart/>*/}
+                                {/*<div className="row">*/}
+                                {/*    <TopSmallChart heading="Top Buyers"/>*/}
+                                {/*    <TopSmallChart heading="Top Sellers"/>*/}
+                                {/*    <TopSmallChart heading="Top Movers"/>*/}
+                                {/*</div>*/}
+                                {/*<div className="row mb-3">*/}
+                                {/*    <HundredTrades/>*/}
+                                {/*</div>*/}
+                                {/*<TopCharts />*/}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </MainTemplate>
     )
 }
